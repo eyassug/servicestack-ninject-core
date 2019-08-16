12cb891ae4d47280c1874c7e105fb7e5a9d4b972
Eyassu Getachew <eyassug@gmail.com>
2019-08-16 23:50:25 +0300

    added MediatR, implemented BuildMediatR extension method

--- src/MediatR.CQRS.Ninject/NinjectKernelExtensions.cs
+++ src/MediatR.CQRS.Ninject/NinjectKernelExtensions.cs
@@ -1,15 +1,27 @@
 ﻿namespace MediatR.CQRS.Ninject
 {
-    using System;
     using System.Reflection;
     using global::Ninject;
     using global::Ninject.Extensions.Conventions;
+    using global::Ninject.Planning.Bindings.Resolvers;
+    using MediatR.Pipeline;
 
     public static class NinjectKernelExtensions
     {
-        public static void BuildMediatR(this IKernel kernel, params Assembly[] assemblies)
+        public static void BuildMediatR(this IKernel kernel, params Assembly[] assembliesWithHandlers)
         {
-            throw new NotImplementedException();
+            kernel.Components.Add<IBindingResolver, ContravariantBindingResolver>();
+
+            kernel.Bind(scan => scan.FromAssemblyContaining<IMediator>().SelectAllClasses().BindDefaultInterface());
+
+            kernel.Bind(scan => scan.From(assembliesWithHandlers).SelectAllClasses().InheritedFrom(typeof(IRequestHandler<,>)).BindAllInterfaces());
+            kernel.Bind(scan => scan.From(assembliesWithHandlers).SelectAllClasses().InheritedFrom(typeof(INotificationHandler<>)).BindAllInterfaces());
+
+            //Pipeline
+            kernel.Bind(typeof(IPipelineBehavior<,>)).To(typeof(RequestPreProcessorBehavior<,>));
+            kernel.Bind(typeof(IPipelineBehavior<,>)).To(typeof(RequestPostProcessorBehavior<,>));
+
+            kernel.Bind<ServiceFactory>().ToMethod(ctx => t => ctx.Kernel.TryGet(t));
         }
     }
 }
