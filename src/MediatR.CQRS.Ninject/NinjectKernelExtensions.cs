f4076e35cfb5cd40404e235ef2642138ea21e9b1
Eyassu Getachew <eyassug@gmail.com>
2019-08-16 23:40:47 +0300

    added NinjectKernelExtensions, test, and BuildMediatR method signature

---
+++ src/MediatR.CQRS.Ninject/NinjectKernelExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace MediatR.CQRS.Ninject
+{
+    using System;
+    using System.Reflection;
+    using global::Ninject;
+    using global::Ninject.Extensions.Conventions;
+
+    public static class NinjectKernelExtensions
+    {
+        public static void BuildMediatR(this IKernel kernel, params Assembly[] assemblies)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/MediatR.CQRS.Ninject.Tests/NinjectKernelExtensionsTests.cs
@@ -0,0 +1,12 @@
+﻿namespace MediatR.CQRS.Ninject.Tests
+{
+    using Xunit;
+    public class NinjectKernelExtensionsTests
+    {
+        [Fact]
+        public void BuildMediatR_ShouldBindInterfaces()
+        {
+
+        }
+    }
+}
